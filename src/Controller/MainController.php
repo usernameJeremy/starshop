574752e74e5974ee6354e8a95475251958efa76d
UsernameJeremy <jeremy_koster@outloook.com>
2025-05-24 14:59:38 +0200

    added curl extension

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,27 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('<strong>Starshop</strong>: you monopoly-busting option for Starship parts!');
+        $starshipCount = 457;
+
+        $myShip = [
+            'name' => 'The Flying Dutchman',
+            'class' => 'Flying Dutchman',
+            'captain' => 'Jeremy The Flying Dutchman',
+            'status' => 'Ready to rumble',
+        ];
+
+        return $this->render('main/homepage.html.twig', [
+            'numberOfStarShips' => $starshipCount,
+            'myShip' => $myShip,
+            ]);
     }
 }
